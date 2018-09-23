ad213a3e20f94bce4e66ca06c46a5eed69ddceb7
artkon <artemkonopelko@gmail.com>
2018-09-23 18:24:15 +0300

    Implement count function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+
+  function NOD(a, b){
+    while(a != 0 && b != 0){
+    	if(a > b){
+    		a = a % b;
+    	} else {
+    		b = b % a;
+    	}
+    }
+    return a + b;
+  }
+
+  var N = 1;
+  for(let i = 0; i < pairs.length; i++){
+  	N *= Math.pow( pairs[i][0], pairs[i][1] )
+  }
+
+  if(N == Infinity || N > 100000000){
+  	console.log("Infinity");
+  	return 0;
+  }
+
+  var result = 0;
+  var tempCount = 0;
+  var mask = s.split('');
+  
+
+  for(let k = 1; k <= N; k++){
+  	for(let j = 0; j < mask.length; j++){
+  		if( NOD(k + j, N) == 1 && mask[j] == 1 ){
+  			tempCount++;
+  			// console.log('k: ' + k + ', j:' + j + ', NOD:' + NOD(k + j, N) + ', N:' + N);
+  		} else if( NOD(k + j, N) > 1 && mask[j] == 0 ){
+  			tempCount++;
+  			// console.log('k: ' + k + ', j:' + j + ', NOD:' + NOD(k + j, N) + ', N:' + N);
+  		}
+  	}
+  	if(tempCount == mask.length){
+  		result++;
+  		tempCount = 0;
+  	} else {
+  		tempCount = 0;
+  	}
+  }
+
+  return result;
+
 }
